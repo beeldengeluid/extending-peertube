0c0ec6cb107983ee747134f22fc95de94ebb00d9
Frank Sträter <fstrater@beeldengeluid.nl>
2019-08-06 17:28:26 +0200

    More work in progress

--- upload.php
+++ upload.php
@@ -60,24 +60,18 @@
 
 	// Upload video
 
-	$url = $api_url.'/videos/imports'; 
+	$url = $api_url.'/videos/upload'; 
 
-	$array_video['name'] = 'Test2';
+	$array_video['name'] = 'Een imitatie van Charlie Chaplin';
 	$array_video['channelId'] = 2;
-	$array_video['targetUrl'] = 'https://www.openbeelden.nl/files/56/56216.56205.BG_9541.ogv';
+	$array_video['videofile'] = curl_file_create('/home/frank/Downloads/WEEKNUMMER340-HRE0000D5AF_579560_634720.mp4');
 	$array_video['language'] = 'nl';
 	$array_video['licence'] = '7';
 	$array_video['privacy'] = '1';
 	$array_video['commentsEnabled'] = '1';
 	$array_video['description'] = "Weeknummer 34-05\nBioscoopjournaals waarin Nederlandse onderwerpen van een bepaalde week worden gepresenteerd.\n\nVoorbereidingen voor carnaval in de dierenwereld: een paard imiteert Charlie Chaplin. -Div. shots als Charlie Chaplin vermomd paard; -div. titel(s)/tussentitel(s).\n\nPolygoon-Profilti (producent) / Nederlands Instituut voor Beeld en Geluid (beheerder)";
 
-	//$array_video['originallyPublishedAt'] = '1934-01-29';
-
-
-	// https://www.openbeelden.nl/files/11/53/1153654.1153651.WEEKNUMMER483-HRE0001B08B_2810800_2962360.mp4; https://www.openbeelden.nl/files/11/53/1153656.1153651.WEEKNUMMER483-HRE0001B08B_2810800_2962360.mp4; https://www.openbeelden.nl/files/11/53/1153651.WEEKNUMMER483-HRE0001B08B_2810800_2962360.mp4; https://www.openbeelden.nl/files/11/53/1153658.1153651.WEEKNUMMER483-HRE0001B08B_2810800_2962360.webm; https://www.openbeelden.nl/files/11/53/1153660.1153651.WEEKNUMMER483-HRE0001B08B_2810800_2962360.ogv; https://www.openbeelden.nl/files/11/53/1153662.1153651.WEEKNUMMER483-HRE0001B08B_2810800_2962360.ogv; https://www.openbeelden.nl/files/11/53/1156397.1153651.WEEKNUMMER483_HRE0001B08B_2810800_2962360.m3u8; https://www.openbeelden.nl/images/1153894/Wereldconferentie_der_Kerken_openingsdienst_in_de_Nieuwe_Kerk_zitting_in_het_Concertgebouw_%281_15%29.png
-
-
-	// https://www.openbeelden.nl/files/56/56219.56205.BG_9541.mp4; https://www.openbeelden.nl/files/56/56213.56205.BG_9541.ogv; https://www.openbeelden.nl/files/56/56216.56205.BG_9541.ogv; https://www.openbeelden.nl/files/56/56225.56205.BG_9541.m3u8; https://www.openbeelden.nl/files/56/56222.56205.BG_9541.ts; https://www.openbeelden.nl/files/56/56209.56205.BG_9541.mpeg; https://www.openbeelden.nl/files/56/56205.BG_9541.mpg; https://www.openbeelden.nl/images/604258/Persoonlijke_kampioenschappen_beugelen_in_Stramproy_%280_41%29.png
+	$array_video['originallyPublishedAt'] = '1934-01-29T12:00:00.000Z';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
