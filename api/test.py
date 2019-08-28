fd37e566c0df8b13a86b70856cd8c242ec99ef84
Frank Sträter <fstrater@beeldengeluid.nl>
2019-08-28 11:05:32 +0200

    Update Python API tester

--- api/test.py
+++ api/test.py
@@ -2,8 +2,40 @@
 
 import requests, json
 
+# API vars
+
+api_url = 'https://peertube.beeldengeluid.nl/api/v1'
+api_user = 'nisv'
+api_pass = 'openbeelden'
+channel_id = 2
+
+# Get client
+
+response = requests.get(api_url + '/oauth-clients/local')
+data = response.json()
+client_id = data['client_id']
+client_secret = data['client_secret']
+
+# Get user token
+
+data = {
+	'client_id': client_id,
+	'client_secret': client_secret,
+	'grant_type': 'password',
+	'response_type': 'code',
+	'username': api_user,
+	'password': api_pass
+}
+
+response = requests.post(api_url + '/users/token', data=data)
+data = response.json()
+token_type = data['token_type']
+access_token = data['access_token']
+
+# Authorization header
+
 headers = {
-    'Authorization': 'Bearer fd476f18fb3df5c092302e1fc6518efc3e991764'
+	'Authorization': token_type + ' ' + access_token
 }
 
 # files = {
@@ -19,13 +51,13 @@ headers = {
 # print json.dumps(data, indent=2)
 
 data = {
-    'channelId': 2,
+    'channelId': channel_id,
     'name': 'Test',
-    'tags': 'tentoon',
+    'tags[0]': 'tentoon',
     'targetUrl': 'https://www.openbeelden.nl/files/11/52/1152271.1152266.WEEKNUMMER481-HRE0000CABE_2369000_2420000.mp4'
 }
 
-response = requests.post('https://peertube.beeldengeluid.nl/api/v1/videos/imports', headers=headers, data=data)
+response = requests.post(api_url + '/videos/imports', headers=headers, data=data)
 data = response.json()
 
 print(response.status_code)
